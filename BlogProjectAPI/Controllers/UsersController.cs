d659ec9c68dd7bfda5db202172aed4f7215cc214
William Fred <williamfred278@gmail.se>
2023-01-12 17:08:33 +0100

    Add project files.

---
+++ BlogProjectAPI/Controllers/UsersController.cs
@@ -0,0 +1,66 @@
+﻿using BlogProjectAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UsersController(UserManager<IdentityUser> userManager)
+        {
+            this._userManager = userManager;
+        }
+
+
+        // GET: api/Users/username
+        [HttpGet("{username}")]
+        public async Task<ActionResult<User>> GetUser(string username)
+        {
+            IdentityUser user = await _userManager.FindByNameAsync(username);
+
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            return new User
+            {
+                UserName = user.UserName,
+                Email = user.Email
+            };
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _userManager.CreateAsync(new IdentityUser()
+            {
+                UserName = user.UserName,
+                Email = user.Email,
+            }, user.Password
+            );
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            user.Password = null;
+            return Created("", user);
+        }
+    }
+}
---
+++ BlogProjectAPI/Models/BlogContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogProjectAPI.Models
+{
+    public class BlogContext : IdentityUserContext<IdentityUser>
+    {
+        public BlogContext(DbContextOptions<BlogContext> options)
+           : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
