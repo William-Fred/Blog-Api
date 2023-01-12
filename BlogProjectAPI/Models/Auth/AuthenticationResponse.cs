a8c56645b56027eea1e30bdcd43c5292d69bc8a8
William Fred <williamfred278@gmail.se>
2023-01-12 17:45:49 +0100

    classes for auth response and req

---
+++ BlogProjectAPI/Models/Auth/AuthenticationResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BlogProjectAPI.Models.Auth
+{
+    public class AuthenticationResponse
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
